ca81a812dc72725d148a7e8a5a9b1cd248d237d4
Manuel Barzi <manuelbarzi@Manuels-MacBook-Pro.local>
2016-04-05 11:16:13 +0200

    update content

--- dist/WingsJS/wings-0.1.0.js
+++ dist/WingsJS/wings-0.1.0.js
@@ -176,13 +176,13 @@ var Wings;
 	var Class;
 	(function() {
 
-		// The base Class implementation (does nothing)
-		Class = function Class() {
-		};
-
 		var initializing = false, fnTest = /xyz/.test(function() {
 			xyz;
 		}) ? /\b_super\b/ : /.*/;
+
+		// The base Class implementation (does nothing)
+		Class = function Class() {
+		};
 
 		// Create a new Class that inherits from this class
 		Class.extend = function(prop) {
@@ -229,9 +229,9 @@ var Wings;
 			}
 
 			// Force eval to correctly inherit the name of the
-			// constructor name (init), otherwise it is not possible to set it
-			// (the name of a function is read-only and it can only be defined
-			// at the time it is declared; see
+			// constructor name (init), otherwise is not possible to set it (the
+			// name of a function is read-only and it can only be defined at the
+			// time it is declared; see
 			// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function)
 			eval('Class = function '
 					+ (prop.init && prop.init.name ? prop.init.name : 'Class')
